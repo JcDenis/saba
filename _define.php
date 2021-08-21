a214e4077bf940e936d781e742aa8efb9a6d3f2c
Jean-Christian Denis <contact@jcdenis.fr>
2021-08-21 22:12:28 +0200

    Initial commit

---
+++ _define.php
@@ -0,0 +1,37 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+#
+# This file is part of saba, a plugin for Dotclear 2.
+# 
+# Copyright (c) 2009-2013 Jean-Christian Denis and contributors
+# contact@jcdenis.fr
+# 
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK ------------------------------------
+
+if (!defined('DC_RC_PATH')) {
+
+	return null;
+}
+
+$this->registerModule(
+	/* Name */
+	"saba",
+	/* Description*/
+	"Search across blog's archive",
+	/* Author */
+	"Jean-Christian Denis",
+	/* Version */
+	'2013.10.29',
+	/* Properies */
+	array(
+		'permissions' => 'admin',
+		'type' => 'plugin',
+		'dc_min' => '2.6',
+		'support' => 'http://jcd.lv/q=saba',
+		'details' => 'http://plugins.dotaddict.org/dc2/details/saba'
+	)
+);
---
+++ locales/fr/main.lang.php
@@ -0,0 +1,94 @@
+<?php
+// Language: Français 
+// Module: saba - 2013.10.28
+// Date: 2013-10-29 08:06:52 
+// Translated with dcTranslater - 2013.05.11 
+
+#_config.php:70
+$GLOBALS['__l10n']['Configuration has been successfully updated.'] = 'La configuration a été mise à jour avec succés.';
+
+#_config.php:93
+$GLOBALS['__l10n']['Enable extension on this blog'] = 'Activer l\'extension sur ce blog';
+
+#_config.php:98
+$GLOBALS['__l10n']['Advanced search'] = 'Recherche avancée';
+
+#_config.php:105
+$GLOBALS['__l10n']['Disable filter on post options'] = 'Désactiver le filtre des options de billet';
+
+#_config.php:112
+$GLOBALS['__l10n']['Disable filter on order'] = 'Désactiver le filtre d\'ordre';
+
+#_config.php:119
+$GLOBALS['__l10n']['Disable filter on age'] = 'Désactiver le filtre de date';
+
+#_config.php:126
+$GLOBALS['__l10n']['Disable filter on categories'] = 'Désactiver le filtre de catégories';
+
+#_config.php:133
+$GLOBALS['__l10n']['Disable filter on authors'] = 'Désactiver le filtre d\'auteurs';
+
+#_config.php:140
+$GLOBALS['__l10n']['Disable filter on post types'] = 'Désactiver le filtre de type de billets';
+
+#_public.php:345
+$GLOBALS['__l10n']['Filter options'] = 'Options du filtre';
+
+#_public.php:365
+$GLOBALS['__l10n']['Reverse order'] = 'Inverser l\'ordre';
+
+#_public.php:367
+$GLOBALS['__l10n']['Filter order'] = 'Options de tri';
+
+#_public.php:388
+$GLOBALS['__l10n']['Filter by category'] = 'Filter par catégorie';
+
+#_public.php:409
+$GLOBALS['__l10n']['Filter by type'] = 'Filtrer par type';
+
+#_public.php:425
+$GLOBALS['__l10n']['Filter by age'] = 'Filtrer par date';
+
+#_public.php:446
+$GLOBALS['__l10n']['Filter by author'] = 'Filtrer par auteur';
+
+#_public.php:477
+$GLOBALS['__l10n']['With comments'] = 'Avec commentaires';
+
+#_public.php:478
+$GLOBALS['__l10n']['With trackbacks'] = 'Avec rétroliens';
+
+#_public.php:490
+$GLOBALS['__l10n']['Comments count'] = 'Nombre de commentaires';
+
+#_public.php:491
+$GLOBALS['__l10n']['Trackbacks count'] = 'Nombre de rétroliens';
+
+#_public.php:498
+$GLOBALS['__l10n']['All'] = 'Tous';
+
+#_public.php:499
+$GLOBALS['__l10n']['Less than a month'] = 'Moins d\'un mois';
+
+#_public.php:500
+$GLOBALS['__l10n']['From 1 to 6 month'] = 'De 1 à 6 mois';
+
+#_public.php:501
+$GLOBALS['__l10n']['From 6 to 12 month'] = 'De 6 à 12 mois';
+
+#_public.php:502
+$GLOBALS['__l10n']['More than a year'] = 'Plus d\'un an';
+
+#_public.php:511
+$GLOBALS['__l10n']['Poll'] = 'Sondage';
+
+#_public.php:512
+$GLOBALS['__l10n']['Event'] = 'Événement';
+
+$GLOBALS['__l10n']['Search across blog\'s archive'] = 'Rechercher dans les archives';
+
+$GLOBALS['__l10n']['Please select a request and options to search on'] = 'Merci de formuler une requête et les options de recherche';
+
+$GLOBALS['__l10n']['Suggested items'] = 'Articles suggérés';
+
+?>
