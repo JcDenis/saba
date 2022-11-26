8e8ff0370e74ba1b917cabcbdf23419798ee572f
Jean-Christian Denis <contact@jcdenis.fr>
2022-11-26 15:09:42 +0100

    use anonymous function

--- _admin.php
+++ _admin.php
@@ -14,45 +14,30 @@ if (!defined('DC_CONTEXT_ADMIN')) {
     return;
 }
 
-# settings namespace
-dcCore::app()->blog->settings->addNamespace('saba');
-
 # widget
 require_once __DIR__ . '/_widgets.php';
 
+# settings namespace
+dcCore::app()->blog->settings->addNamespace('saba');
+
 # behaviors
-dcCore::app()->addBehavior(
-    'adminBlogPreferencesFormV2',
-    ['adminSaba', 'adminBlogPreferencesForm']
-);
-dcCore::app()->addBehavior(
-    'adminBeforeBlogSettingsUpdate',
-    ['adminSaba', 'adminBeforeBlogSettingsUpdate']
-);
+dcCore::app()->addBehavior('adminBlogPreferencesFormV2', function ($blog_settings) {
+    echo
+    '<div class="fieldset">' .
+    '<h4 id="saba_params">' . __('Search Across Blog Archive') . '</h4>' .
+    '<p><label class="classic">' .
+    form::checkbox('saba_active', '1', (bool) $blog_settings->saba->active) .
+    __('Enable advanced search on this blog') . '</label></p>' .
+    '<p><label class="classic">' .
+    form::checkbox('saba_error', '1', (bool) $blog_settings->saba->error) .
+    __('Enable suggestion for page 404') . '</label></p>' .
+    '<p class="form-note">' .
+    __('This suggests visitors some posts on page 404.') .
+    '</p>' .
+    '</div>';
+});
 
-# add settings to admin blog pref page
-class adminSaba
-{
-    public static function adminBlogPreferencesForm($blog_settings)
-    {
-        echo
-        '<div class="fieldset">' .
-        '<h4 id="saba_params">' . __('Search Across Blog Archive') . '</h4>' .
-        '<p><label class="classic">' .
-        form::checkbox('saba_active', '1', (bool) $blog_settings->saba->active) .
-        __('Enable advanced search on this blog') . '</label></p>' .
-        '<p><label class="classic">' .
-        form::checkbox('saba_error', '1', (bool) $blog_settings->saba->error) .
-        __('Enable suggestion for page 404') . '</label></p>' .
-        '<p class="form-note">' .
-        __('This suggests visitors some posts on page 404.') .
-        '</p>' .
-        '</div>';
-    }
-
-    public static function adminBeforeBlogSettingsUpdate($blog_settings)
-    {
-        $blog_settings->saba->put('active', !empty($_POST['saba_active']));
-        $blog_settings->saba->put('error', !empty($_POST['saba_error']));
-    }
-}
+dcCore::app()->addBehavior('adminBeforeBlogSettingsUpdate', function ($blog_settings) {
+    $blog_settings->saba->put('active', !empty($_POST['saba_active']));
+    $blog_settings->saba->put('error', !empty($_POST['saba_error']));
+});
