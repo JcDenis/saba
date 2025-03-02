cdea766bf6ad326c7931d52208f30fb2487d6107
Jean-Christian Denis <contact@jcdenis.fr>
2025-03-02 20:19:39 +0100

    release 2025.03.02

--- _define.php
+++ _define.php
@@ -17,7 +17,7 @@ $this->registerModule(
     'saba',
     'Search across blog archive',
     'Jean-Christian Denis and Contributors',
-    '2023.11.04',
+    '2025.03.02',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
